060264e3ac3fba25aebd41dfcdf2548949e6a878
Guilherme Mafra da Costa <g.mafra.costa@gmail.com>
2024-06-06 15:07:25 -0300

    Updating the interface all of the commodities

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request, jsonify
 import json
-import time
-import random
+import time as time_module
 from pymongo import MongoClient
 from confluent_kafka.admin import AdminClient
 
@@ -12,45 +11,37 @@ kafka_conf = {
 }
 admin_client = AdminClient(kafka_conf)
 
-
 def list_kafka_topics():
     metadata = admin_client.list_topics(timeout=10)
     return metadata.topics.keys()
 
-
 def get_all_documents_from_collection(collection_name):
     db_client = MongoClient("localhost", 27017)
     mydb = db_client["commodities_db"]
-    prices = [float(doc['price']) for doc in mydb[collection_name].find({}, {'price': 1, '_id': 0})]
-    prices = json.dumps(prices)    
-    return prices
-
+    documents = mydb[collection_name].find({}, {'price': 1, 'time': 1, '_id': 0})
+    data = [{'price': float(doc['price']), 'time': doc['time']} for doc in documents]
+    return data
 
 def generate_data_by_commoditie(topicname):
-
     while True:
-        commodities = get_all_documents_from_collection(topicname) 
-        yield f"data: {commodities}\n\n"
-        time.sleep(2)  
+        commodities = get_all_documents_from_collection(topicname)
+        yield f"data: {json.dumps(commodities)}\n\n"
+        time_module.sleep(2)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route('/commoditie/<topicname>')
-# def get_commoditie(topicname):
-
-#     available_topics = list_kafka_topics()
-#     if topicname not in available_topics:
-#         return f"Commoditie {topicname} not found", 404
-#     print(generate_data_by_commoditie(topicname))
-#     return render_template('index.html', topicname=topicname)
-    
-
 @app.route('/stream')
 def stream():
-    topicname = 'stock-producer'
-    return Response(generate_data_by_commoditie('gold'), mimetype='text/event-stream')
-    
+    commodity = request.args.get('commodity', 'gold')
+    return Response(generate_data_by_commoditie(commodity), mimetype='text/event-stream')
+
+@app.route('/data')
+def data():
+    commodity = request.args.get('commodity', 'gold')
+    prices = get_all_documents_from_collection(commodity)
+    return jsonify(prices)
+
 if __name__ == '__main__':
     app.run(debug=True)
