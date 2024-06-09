93ef432838b6bb31419a2cd6cd54ebbc90167d4d
Guilherme Mafra da Costa <g.mafra.costa@gmail.com>
2024-06-09 16:39:50 -0300

    Removing the scripts

--- scripts/create-topics.sh
+++
@@ -1,4 +0,0 @@
-KAFKA_TOPICS="topic1 topic2 topic3 topic4 topic5"
-for topic in $KAFKA_TOPICS; do
-  kafka-topics --create --topic $topic --bootstrap-server kafka:29092 --partitions 1 --replication-factor 1
-done
